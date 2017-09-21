6609fc244fb0e2c0ab74d59b323b43f83a87ecf2
jdbencke <jdbencke@ncsu.edu>
2017-09-20 20:00:40 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -342,7 +342,9 @@ function drawRandPixelsInInputEllipsoids(context) {
 						intercept = Vector.scale(closeT,D);
 						intercept = Vector.add(eye,intercept);
 					   var xIn = Math.round(w*intercept.x);
+					   xIn = xIn/2;
 					   var yIn = Math.round(h*intercept.y);
+					   yIn = yIn/2;
 				   	drawPixel(imagedata,Math.round(xIn),Math.round(yIn),c);
 					}
 					}
