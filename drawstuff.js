62e52a64c6348bdf07e07629fa637e449cf0ff27
jdbencke <jdbencke@ncsu.edu>
2017-09-21 20:17:36 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -407,6 +407,7 @@ function main() {
     // Get the canvas and context
     var canvas = document.getElementById("viewport");
     var context = canvas.getContext("2d");
+	context = context.scale(1,-1);
 
  
     // Create the image
