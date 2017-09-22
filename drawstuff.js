41b3e2e5b8fb9cad43bfd02566924868cbbc70a8
jdbencke <jdbencke@ncsu.edu>
2017-09-21 21:26:21 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -359,7 +359,7 @@ function drawRandPixelsInInputEllipsoids(context) {
                 } // end for ellipsoids
         } // end for pixels in ellipsoid
         context.putImageData(imagedata, 0, 0);
-	context.scale(-1,1);
+	
 	
     } // end if ellipsoids found
  // end draw rand pixels in input ellipsoids
@@ -410,7 +410,8 @@ function main() {
     // Get the canvas and context
     var canvas = document.getElementById("viewport");
     var context = canvas.getContext("2d");
-	
+	context.translate(0,canvas.height);
+	context.scale(1,-1);
 
  
     // Create the image
